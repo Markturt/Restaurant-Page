2307aa167d489698c3f425231516cf0fa7cd800d
Praise <praisem52@gmail.com>
2024-01-21 14:26:30 -1100

    About page File Added
    The about page and the subseuent styling file were added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log("Js Speaks");
+import "./style.css";
+import {about} from "./About.js";
+
+const content=document.getElementById("content");
+
+const title=document.createElement("h1");
+title.id="title";
+
+title.textContent="Mark's Kitchen"
+
+const tab=document.createElement("div");
+tab.classList.add("tab");
+
+const tablink=document.getElementsByClassName("tablink");
+
+tab.innerHTML=`
+            <button class="tablink" id="about">About</button>
+            <button class="tablink" id="order">Order</button>
+            <button class="tablink" id="menu">Menu</button>
+            `;
+
+content.appendChild(title);
+content.appendChild(tab);
+
+
+for(let link of tablink){
+        link.addEventListener("click",(e)=>{
+            if(e.target.id=="about"){
+                content.appendChild(about());
+            }
+            if(e.target.id=="order"){
+                console.log(e.target)
+                console.log("Order");
+            }
+            if(e.target.id=="menu"){
+                console.log("Menu");
+            }
+        })
+}
